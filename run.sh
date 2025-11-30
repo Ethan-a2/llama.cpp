217f7adedbb491bd6b20f5c69640c57563f85ba6
kevin <kevin@163.com>
2025-11-30 17:00:40 +0800

    PERF run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 cmake --build build-snapdragon --target ggml-hexagon --verbose
 adb push ./build-snapdragon/bin/libggml-hexagon.so /data/local/tmp/llama.cpp/lib/libggml-hexagon.so
-V=1 M=LFM2-350M-Q4_0.gguf D=HTP0 ./scripts/snapdragon/adb/run-cli.sh -no-cnv -p \"1+1=?\" --verbose
+V=0 PROF=1 M=LFM2-350M-Q4_0.gguf D=HTP0 ./scripts/snapdragon/adb/run-cli.sh -no-cnv -p \"1+1=?\" --verbose
+# V=1 PROF=1 M=LFM2-350M-Q4_0.gguf D=HTP0 ./scripts/snapdragon/adb/run-cli.sh -no-cnv -p \"1+1=?\" --verbose
